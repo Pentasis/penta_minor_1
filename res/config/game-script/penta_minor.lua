21f0a04b3bbda965d356b8e266187d3fb281c562
Pentasis <7046024+Pentasis@users.noreply.github.com>
2022-05-17 11:42:47 +0200

    refactor update function

--- res/config/game-script/penta_minor.lua
+++ res/config/game-script/penta_minor.lua
@@ -1,14 +1,12 @@
 local isGamePaused = false
 
-local function startPaused()
-    if game ~= nil and not isGamePaused then
-        game.interface.setGameSpeed(0)
-        isGamePaused = true
-    end
-end
-
 function data()
     return {
-        update = startPaused,
+        update = function()
+            if game ~= nil and not isGamePaused then
+                game.interface.setGameSpeed(0)
+                isGamePaused = true
+            end
+        end,
     }
 end
