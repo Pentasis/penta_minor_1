f021dbdd7510680c305f30103ab6939992ef5b96
Pentasis <7046024+Pentasis@users.noreply.github.com>
2022-05-29 11:10:03 +0200

    cleanup

--- res/scripts/penta_minor/industry.lua
+++ res/scripts/penta_minor/industry.lua
@@ -3,19 +3,6 @@ local industry = {}
 local factorWorkers = 4
 local factorProduction = 4
 
-local function dump(o)
-    if type(o) == 'table' then
-        local s = '{ '
-        for k, v in pairs(o) do
-            if type(k) ~= 'number' then k = '"' .. k .. '"' end
-            s = s .. '[' .. k .. '] = ' .. dump(v) .. ','
-        end
-        return s .. '} '
-    else
-        return tostring(o)
-    end
-end
-
 local function changeIndustry(filename, factory)
     local originalUpdateFn = factory.updateFn
 
